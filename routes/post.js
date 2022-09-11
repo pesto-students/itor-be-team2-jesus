95fe0e6058abc0b3ce937c550928bb71a9749a44
mitcareem <mitcareem@gmail.com>
2022-09-11 10:42:42 +0530

    fix: create comment route

--- routes/post.js
+++ routes/post.js
@@ -7,7 +7,7 @@ const { isLoggedInUser } = require("../middlewares/user");
 
 router.route("/create/post").post(isLoggedInUser, createPost);
 router.route("/posts/search").get(isLoggedInUser, getAllPosts);
-router.route("/create/comment").put(isLoggedInUser, createComment);
+router.route("/create/comment/:id").put(isLoggedInUser, createComment);
 
 
 module.exports = router;
